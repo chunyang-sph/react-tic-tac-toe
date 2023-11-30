6f32bd7a0df559559327cab5cccc818d25c846ef
chunyang <chunyang@sph.com.sg>
2023-11-30 11:12:54 +0800

    Update test

--- src/components/gameSquare/gameSquare.test.js
+++ src/components/gameSquare/gameSquare.test.js
@@ -1,1 +1,27 @@
-test.skip("temp skip game square testing", () => {})
+import { act, render, screen } from "@testing-library/react"
+import GameSquare from "./gameSquare"
+import userEvent from "@testing-library/user-event"
+
+describe("When Clicked, x or o appears", () => {
+	test("Click on X", () => {
+		render(
+			<GameSquare
+				id={1}
+				turn={true}
+				changeTurn={() => {}}
+				markSquare={() => {}}
+			/>
+		)
+
+		// when user click on game square
+		const gameSquare = screen.queryByText("X")
+
+		act(() => {
+			userEvent.click(gameSquare)
+		})
+
+		// then x appears on game
+		const gameSquareWithX = screen.findByText("x")
+		expect(gameSquareWithX).toBeReceived
+	})
+})
